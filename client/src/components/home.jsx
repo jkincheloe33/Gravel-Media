ffe9539875e7a3a72a2395e284790021a2e0e0d5
Josh Kincheloe <jkincheloe33@gmail.com>
2018-06-18 16:42:15 -0500

    Added Jumbotron from bootstrap and added styles.css.

--- client/src/components/home.jsx
+++ client/src/components/home.jsx
@@ -1,15 +1,14 @@
-import React, { Component } from 'react';
-import { render } from 'react-dom';
+import React, { Component, Fragment } from 'react';
 
-class Home extends Component {
-
-    constructor(props) {
-        super(props);
-    }
-
-    render() {
-        return <h1>Hello World!</h1>;
-    }
+const Home = () => {
+    return (
+        <div className="jumbotron jumbotron-fluid bg-dark text-white mb-0 jumbotron-header">
+            <div className="header-container pl-4 pr-4">
+                <h1 className="">Gravel Media</h1>
+                <p className="">A clever creative motto goes here.</p>
+            </div>
+        </div>
+    );
 }
 
 export default Home;
