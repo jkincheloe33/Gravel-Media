209fb8a682c211fb7eadca1c9d0e9732f9cc5bb0
Josh Kincheloe <jkincheloe33@gmail.com>
2018-06-18 21:38:04 -0500

    Added favicon to site head.

--- client/src/components/home.jsx
+++ client/src/components/home.jsx
@@ -2,7 +2,7 @@ import React, { Component, Fragment } from 'react';
 
 const Home = () => {
     return (
-        <div className="jumbotron jumbotron-fluid bg-dark text-white mb-0 jumbotron-header animatedFive infinite pulse">
+        <div className="jumbotron jumbotron-fluid bg-dark text-white mb-0 p-0 jumbotron-header animatedFive infinite pulse">
             <div className="header-container">
                 <h1 className="">Gravel Media</h1>
                 <p className="">A clever creative motto goes here.</p>
