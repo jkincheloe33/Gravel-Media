774ffbb42cf00918b2348273773ebf18800ce189
Josh Kincheloe <jkincheloe33@gmail.com>
2018-06-19 19:32:45 -0500

    Finalized animation and styling for About.jsx.

--- client/src/components/about.jsx
+++ client/src/components/about.jsx
@@ -3,14 +3,13 @@ import React, { Fragment } from 'react';
 const About = () => {
     return (
         <Fragment>
-            <a name="about"></a>
-            <div className="mb-0 jumbotron-about">
+            <div className="mb-0 jumbotron-about animated fadeIn">
                 <div className="about-container pl-1 pr-1">
-                    <h1 className="">
+                    <h1 className="animated fadeInDown">
                         Let us bring your vision to life.
                     </h1>
-                    <p className="">
-                        Gravel Media is a full service design agency. From branding to web applications, we can take your idea and develope it to suit your needs.
+                    <p className="animatedFour fadeInUp">
+                        Gravel Media is a full service design agency. From branding to web and mobile applications, we can your idea and develope it to suit your needs.
                         </p>
                 </div>
             </div>
