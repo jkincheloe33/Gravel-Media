4a806911f9dd89fbf42a41c10898349c647e460c
Josh Kincheloe <jkincheloe33@gmail.com>
2018-06-20 12:32:04 -0500

    Finalized animation on work page.

--- client/src/components/work.jsx
+++ client/src/components/work.jsx
@@ -5,28 +5,36 @@ const Work = () => {
     return (
         <Fragment>
             <div className="container-fluid work-container">
-                <div className="row pl-1 pr-1 m-auto work-row">
-                    <div className="col-6 col-sm-4 col-md-4 p-0 animatedFour slideInRight work-item">
-                        <img src="../../img/glasslands2.png" />
-                        <div className="overlay">
-                            <p>Glasslands</p>
+                <div className="work-row m-auto">
+                    <div className="row pl-1 pr-1 m-auto work-images">
+                        <div className="col-6 col-sm-4 col-md-4 p-0 animated work-item">
+                            <a href="http://glasslands.co" target="_blank">
+                                <img src="../../img/glasslands2.png" className="animatedEight" />
+                                <div className="overlay p-1">
+                                    <p>Glasslands</p>
+                                </div>
+                            </a>
                         </div>
-                    </div>
-                    <div className="col-6 col-sm-4 col-md-4 p-0 animated slideInLeft work-item">
-                        <img src="../../img/glassarrow2.png" />
-                        <div className="overlay">
-                            <p>Glass Arrow Audio</p>
+                        <div className="col-6 col-sm-4 col-md-4 p-0 animated slideInLeft work-item">
+                            <img src="../../img/glassarrow2.png" className="animatedSeven" />
+                            <a href="http://glassarrowaudio.herokuapp.com" target="_blank">
+                                <div className="overlay p-1">
+                                    <p>Glass Arrow</p>
+                                </div>
+                            </a>
                         </div>
-                    </div>
-                    <div className="col-6 col-sm-4 col-md-4 p-0 animatedSix slideInRight work-item">
-                        <img src="../../img/jk.png" />
-                        <div className="overlay">
-                            <p>Josh Kincheloe</p>
+                        <div className="col-6 col-sm-4 col-md-4 p-0 animated slideInRight work-item">
+                            <img src="../../img/jk.png" className="animatedEight" />
+                            <a href="http://jkincheloe33.github.io/" target="_blank">
+                                <div className="overlay">
+                                    <p>Josh Kincheloe</p>
+                                </div>
+                            </a>
                         </div>
                     </div>
                 </div>
             </div>
-        </Fragment>
+        </Fragment >
     )
 }
 
