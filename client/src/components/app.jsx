f0a8bc41033a2cce4c582d5b69481f09d34af00c
Josh Kincheloe <jkincheloe33@gmail.com>
2018-06-19 21:05:18 -0500

    Added work.jsx. Laid out the structure for Work page.

--- client/src/components/app.jsx
+++ client/src/components/app.jsx
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Route, Switch, Link } from 'react-router-dom';
 import Home from './home';
 import Nav from './nav';
 import About from './about';
+import Work from './work';
 import GoodbyeWorld from './goodbye';
 
 class Navigation extends Component {
@@ -16,6 +17,7 @@ class Navigation extends Component {
                     <Switch>
                         <Route exact path="/" component={Home} />
                         <Route path="/about" component={About} />
+                        <Route path="/work" component={Work} />
                     </Switch>
                 </Fragment>
             </Router>
---
+++ client/src/components/work.jsx
@@ -0,0 +1,32 @@
+import React, { Component, Fragment } from 'react';
+
+const Work = () => {
+
+    return (
+        <Fragment>
+            <div className="work-container">
+
+                <div className="workOne">
+
+                </div>
+                <div className="workTwo">
+
+                </div>
+                <div className="workThree">
+
+                </div>
+            </div>
+            <div className="container-fluid contact-container p-3">
+                <div className="row">
+                    <div className="col-4 col-sm-4 col-md-4">
+                        <img src="../../img/logo.png" />
+                    </div>
+                    <div className="col-8 col-sm-4 col-md-8">
+                    </div>
+                </div>
+            </div>
+        </Fragment>
+    )
+}
+
+export default Work;
