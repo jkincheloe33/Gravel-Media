54632aa6fb0c54ee4330780ec9303b2324553b08
Josh Kincheloe <jkincheloe33@gmail.com>
2018-06-21 16:52:52 -0500

    Added particle animation to Home page.

--- client/src/components/home.jsx
+++ client/src/components/home.jsx
@@ -1,16 +1,27 @@
 import React, { Component, Fragment } from 'react';
 
-const Home = () => {
-    return (
-        <div className="animated fadeIn">
-            <div className="jumbotron jumbotron-fluid bg-dark text-white mb-0 p-0 jumbotron-header animatedFive infinite pulse">
-                <div className="animatedFive fadeIn header-container">
-                    <h1 className="">Gravel Media</h1>
-                    {/* <p className="">Design. Create. Develop. </p> */}
+class Home extends Component {
+    constructor(props) {
+        super(props);
+    }
+
+    componentDidMount() {
+        particlesJS.load('particles-home', '../../particles/particlesTwo.json', function () {
+            console.log('particlesTwo.json loaded...');
+        });
+    }
+
+    render() {
+        return (
+            <div className="animated fadeIn">
+                <div className="jumbotron jumbotron-fluid bg-dark text-white mb-0 p-0 jumbotron-header animatedFive infinite pulse" id="particles-home">
+                    <div className="animatedFive fadeIn header-container">
+                        <h1 className="">Gravel Media</h1>
+                    </div>
                 </div>
             </div>
-        </div>
-    );
+        );
+    }
 }
 
 export default Home;
