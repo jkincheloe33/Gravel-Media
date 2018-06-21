5842c7478b13bc82a3d0ee6a057ed76e8f7cdf77
Josh Kincheloe <jkincheloe33@gmail.com>
2018-06-21 16:11:58 -0500

    Added particles.js plugin.

--- client/src/components/about.jsx
+++ client/src/components/about.jsx
@@ -1,20 +1,32 @@
-import React, { Fragment } from 'react';
+import React, { Component, Fragment } from 'react';
 
-const About = () => {
-    return (
-        <Fragment>
-            <div className="mb-0 jumbotron-about animated fadeIn">
-                <div className="about-container pl-1 pr-1">
-                    <h1 className="animated fadeInDown">
-                        Let us bring your vision to life.
-                    </h1>
-                    <p className="animatedFour fadeInUp">
-                        Gravel Media is a full service design agency. From branding to web and mobile applications, we take your idea and develop it to suit your needs.
-                        </p>
+class About extends Component {
+    constructor(props) {
+        super(props);
+    }
+
+    componentDidMount() {
+        particlesJS.load('particles-about', '../../particles/particlesOne.json', function () {
+            console.log('particlesOne.json loaded...');
+        });
+    }
+
+    render() {
+        return (
+            <Fragment>
+                <div className="mb-0 jumbotron-about animated fadeIn" id="particles-about">
+                    <div className="about-container pl-1 pr-1">
+                        <h1 className="animated fadeInDown">
+                            Let us bring your vision to life.
+                        </h1>
+                        <p className="animatedFour fadeInUp">
+                            Gravel Media is a full service design agency. From branding to web and mobile applications, we take your idea and develop it to suit your needs.
+                            </p>
+                    </div>
                 </div>
-            </div>
-        </Fragment>
-    );
+            </Fragment>
+        );
+    }
 }
 
 export default About;
