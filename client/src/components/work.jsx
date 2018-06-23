02d5af0c6162d90df6866f5ce662d339d1f64578
Josh Kincheloe <jkincheloe33@gmail.com>
2018-06-23 16:11:43 -0500

    Restructured Work.jsx with all new images and table layout.

--- client/src/components/work.jsx
+++ client/src/components/work.jsx
@@ -4,32 +4,35 @@ const Work = () => {
 
     return (
         <Fragment>
-            <div className="container-fluid work-container animated fadeIn">
-                <div className="work-row m-auto">
-                    <div className="row pl-1 pr-1 m-auto work-images">
-                        <div className="col-6 col-sm-4 col-md-4 p-0 animated work-item">
-                            <a href="http://glasslands.co" target="_blank">
-                                <img src="../../img/glasslands2.png" className="animatedEight" />
-                                <div className="overlay p-1">
-                                    <p>Glasslands</p>
-                                </div>
-                            </a>
+            <div className="container-fluid work-container animated fadeIn p-0">
+                <div className="work-row">
+                    <div className="row m-auto work-images">
+                        <div className="col-8 p-0">
+                            <img src="../../img/glasslands.jpg" />
                         </div>
-                        <div className="col-6 col-sm-4 col-md-4 p-0 animated slideInLeft work-item">
-                            <img src="../../img/glassarrow2.png" className="animatedSeven" />
-                            <a href="http://glassarrowaudio.herokuapp.com" target="_blank">
-                                <div className="overlay p-1">
-                                    <p>Glass Arrow</p>
-                                </div>
-                            </a>
+                        <div className="col-4 p-0 work-item">
+                            <p>
+                                Glasslands <br />
+                                Full site for Nashville, TN based band.
+                            </p>
                         </div>
-                        <div className="col-6 col-sm-4 col-md-4 p-0 animated slideInRight work-item">
-                            <img src="../../img/jk.png" className="animatedEight" />
-                            <a href="http://jkincheloe33.github.io/" target="_blank">
-                                <div className="overlay">
-                                    <p>Josh Kincheloe</p>
-                                </div>
-                            </a>
+                        <div className="col-4 p-0 work-item">
+                            <p className="JK-work">
+                                Josh Kincheloe <br />
+                                Full site for Nashville, TN based developer.
+                            </p>
+                        </div>
+                        <div className="col-8 p-0">
+                            <img src="../../img/jk.jpg" />
+                        </div>
+                        <div className="col-8 p-0">
+                            <img src="../../img/glassarrow.jpg" />
+                        </div>
+                        <div className="col-4 p-0 work-item">
+                            <p>
+                                Glass Arrow Audio <br />
+                                Full site for Toledo, OH based studio.
+                            </p>
                         </div>
                     </div>
                 </div>
