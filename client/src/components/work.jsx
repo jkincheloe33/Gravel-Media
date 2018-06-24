300ebe40b785b3cd55601f0573a44fa523e1e1f0
Josh Kincheloe <jkincheloe33@gmail.com>
2018-06-24 10:53:49 -0500

    Adjust changed div backgrounds to white. Adjusted footer height and centered the content.

--- client/src/components/work.jsx
+++ client/src/components/work.jsx
@@ -7,51 +7,53 @@ const Work = () => {
             <div className="container-fluid work-container animated fadeIn p-0">
                 <div className="work-row">
                     <div className="row m-auto work-images">
-                        <div className="col-8 p-0">
+                        <div className="col-8 p-0 animated slideInLeft">
                             <img src="../../img/glasslands.jpg" />
                         </div>
-                        <div className="col-4 p-0 work-item">
+                        <div className="col-4 p-0 animated slideInLeft work-item">
                             <p>
                                 Glasslands <br />
                                 Full site for Nashville, TN based band.
                             </p>
                         </div>
-                        <div className="col-4 p-0 work-item">
+                        <div className="col-4 p-0 animatedThree slideInRight work-item">
                             <p className="JK-work">
                                 Josh Kincheloe <br />
                                 Full site for Nashville, TN based developer.
                             </p>
                         </div>
-                        <div className="col-8 p-0">
+                        <div className="col-8 p-0 animatedThree slideInRight">
                             <img src="../../img/jk.jpg" />
                         </div>
-                        <div className="col-8 p-0">
+                        <div className="col-8 p-0 animatedFour slideInLeft">
                             <img src="../../img/glassarrow.jpg" />
                         </div>
-                        <div className="col-4 p-0 work-item">
+                        <div className="col-4 p-0 animatedFour slideInLeft work-item">
                             <p>
                                 Glass Arrow Audio <br />
                                 Full site for Toledo, OH based studio.
                             </p>
                         </div>
                         <div className="col-12 work-footer">
-                            <h1>Connect with us!</h1>
-                            <div className="container">
-                                <div className="row w-75 m-auto">
-                                    <div className="col-4 p-0">
-                                        <a className="btn btn-social-icon btn-facebook p-1 animated" href="http://facebook.com/GlassArrowAudio">
-                                            <i className="fab fa-facebook fa-3x"></i>
-                                        </a>
-                                    </div>
-                                    <div className="col-4 p-0">
-                                        <a className="btn btn-social-icon btn-instagram p-1 animated" href="http://instagram.com/glassarrowaudio">
-                                            <i className="fas fa-camera-retro fa-3x"></i>
-                                        </a>
-                                    </div>
-                                    <div className="col-4 p-0">
-                                        <a className="btn btn-social-icon btn-twitter p-1 animated" href="http://twitter.com/jmochabear">
-                                            <i className="fab fa-twitter-square fa-3x"></i>
-                                        </a>
+                            <div className="footer-container">
+                                <h1>Connect with us!</h1>
+                                <div className="container">
+                                    <div className="row w-75 m-auto">
+                                        <div className="col-4 p-0">
+                                            <a className="btn btn-social-icon btn-facebook p-1 animated" href="http://facebook.com/GlassArrowAudio">
+                                                <i className="fab fa-facebook fa-3x"></i>
+                                            </a>
+                                        </div>
+                                        <div className="col-4 p-0">
+                                            <a className="btn btn-social-icon btn-instagram p-1 animated" href="http://instagram.com/glassarrowaudio">
+                                                <i className="fas fa-camera-retro fa-3x"></i>
+                                            </a>
+                                        </div>
+                                        <div className="col-4 p-0">
+                                            <a className="btn btn-social-icon btn-twitter p-1 animated" href="http://twitter.com/jmochabear">
+                                                <i className="fab fa-twitter-square fa-3x"></i>
+                                            </a>
+                                        </div>
                                     </div>
                                 </div>
                             </div>
